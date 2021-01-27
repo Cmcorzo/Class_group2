9100a44d7c8417ffe4ed9d4e2983e1aadf561ab8
cmcorzo <clara_corzo@yahoo.com>
2021-01-27 17:23:15 +0100

    iadded file with codes

---
+++ 3_git.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan 27 14:45:31 2021
+
+@author: cco004
+"""
+
+# An input code that prints in command line which function to plot 
+# and allow to select a plot from the list 
+# (option 1, plot 1, option2, plot 2, option 3 return
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+def plotletter(c):    
+    c = input("Please enter a letter: ")
+    plt.text(0.5, 0.7, c, size=50, rotation=00.,
+        ha="center", va="center",
+        bbox=dict(boxstyle="round",
+                  ec=(1., 0.5, 0.5),
+                  fc=(1., 0.8, 0.8),
+                       )
+             )
+    plt.show()
+
+
+
+decision = float(input('Please select an option: 1, 2 or 3: '))
+
+while decision != 1 and decision !=2:
+    print("You selected 3. No function returns")
+    decision = float(input('Please select an option: 1, 2 or 3: '))
+
+if decision == 1:
+    print("You selected 1, take a look at the plot")
+    # MyfunctionSine(100,1)1
+elif decision == 2:
+    print("You selected 2")
+    plotletter("letter plot")
+
+    
+    
+
+    
+    
+    
